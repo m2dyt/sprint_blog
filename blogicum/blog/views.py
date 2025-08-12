d71aa30ecaa5431f1acc94aa5960895e7cb4ee65
m2dyt <roman16050811@mail.ru>
2025-08-12 22:36:21 +0300

    second commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Главная страница блога")
+
+def post_detail(request, id):
+    return HttpResponse(f"Страница поста с id={id}")
+
+def category_posts(request, category_slug):
+    return HttpResponse(f"Посты в категории: {category_slug}")
+
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def about(request):
+    return HttpResponse("О сайте")
+
+def rules(request):
+    return HttpResponse("Правила сайта")
+
